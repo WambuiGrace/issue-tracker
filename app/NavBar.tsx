f02cbbb86d69f12a6c3d2ae2c74e82157e9c1960
Grace Wambui <wambuigm51@yahoo.com>
2025-01-02 12:54:40 +0300

    Styling NavBar with active link

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,15 @@
+'use client';
+
 import React from 'react'
 import Link from 'next/link'
 import { GiAlienBug } from "react-icons/gi";
+import { usePathname } from 'next/navigation';
+import classnames from 'classnames';
 
 const NavBar = () => {
+  const currentPath = usePathname();
+  
+
     const links = [
         {label: 'Dashboard', href: '/'},
         {label: 'Issues', href: '/issues'}
@@ -11,11 +18,12 @@ const NavBar = () => {
     <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
         {/* Logo icon from react icons */}
         <Link href="/"><GiAlienBug /></Link>
-        <ul className='flex space-x-6'>
-            {links.map(link => <Link 
-            key={link.href} 
-            className='text-zinc-500 hover:text-zinc-800 transition-colors' href={link.href}>{link.label}</Link>)}
-           
+        <ul className="flex space-x-6">
+          {links.map((link) => (
+            <li key={link.href}>
+              <Link className={classnames({ "nav-link": true, "text-zinc-900": link.href === currentPath,})} href={link.href}>{link.label}</Link>
+            </li>
+          ))}
         </ul>
     </nav>
   )
---
+++ app/issues/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const IssuesPage = () => {
+  return (
+    <div>
+      <h1>Issues</h1>
+    </div>
+  )
+}
+
+export default IssuesPage
+
